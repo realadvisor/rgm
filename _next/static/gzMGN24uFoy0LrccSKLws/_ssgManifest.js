96a99a5f0d75d1b004942888a78f2216861825d2
Ivan Starkov <istarkov@gmail.com>
2020-05-05 23:56:55 +0300

    Updates

---
+++ _next/static/gzMGN24uFoy0LrccSKLws/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002F","\u002Fperformance","\u002Fevents","\u002Fgeojson","\u002Fvis-test-n-index","\u002Fgoogle-marker","\u002Fimperative","\u002Fclusters","\u002Freact-markers","\u002Fvis-test-n-uniq"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
