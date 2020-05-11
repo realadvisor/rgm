3011df81cde2ed58d1f96be6c3b14b7d9d6655b1
Ivan Starkov <istarkov@gmail.com>
2020-05-11 14:27:07 +0300

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "f49ab2fd391c402a2fc4";
+/******/ 	var hotCurrentHash = "ab0197e099ba81455d6d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
